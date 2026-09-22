479fa362e927cc15d9dbf93bfac87a96ef83087b
Viren Ribadiya <viren.ribadiya@fero.ai>
2026-09-22 16:34:56 +0530

    different type of caching and handling

---
+++ caching/app/messages/page.js
@@ -0,0 +1,21 @@
+import Messages from '@/components/messages';
+// import { unstable_noStore } from 'next/cache';
+
+// export const revalidate = 5;
+// export const dynamic = 'force-dynamic';
+
+export default async function MessagesPage() {
+  // unstable_noStore();
+  const response = await fetch('http://localhost:8080/messages', {
+    // headers: {
+    //   'X-ID': 'page',
+    // },
+  });
+  const messages = await response.json();
+
+  if (!messages || messages.length === 0) {
+    return <p>No messages found</p>;
+  }
+
+  return <Messages messages={messages} />;
+}
