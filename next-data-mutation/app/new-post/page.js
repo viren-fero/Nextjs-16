be9540c29a237ba127e1a11fb1d794fc02add183
Viren Ribadiya <viren.ribadiya@fero.ai>
2026-09-22 12:58:03 +0530

    revalidation, server actions and data fetching and cloudinary integration

--- next-data-mutation/app/new-post/page.js
+++ next-data-mutation/app/new-post/page.js
@@ -1,30 +1,6 @@
+import { createPost } from "@/actions/posts";
+import PostForm from "@/components/post-form";
+
 export default function NewPostPage() {
-  return (
-    <>
-      <h1>Create a new post</h1>
-      <form>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
-          <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
-          />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
-        </p>
-      </form>
-    </>
-  );
+  return <PostForm action={createPost} />
 }
